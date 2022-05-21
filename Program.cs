669c48e907fe3d26b1e8845a14d424ebb2109c51
Mikhail Vinogradov <m89119750476@gmail.com>
2022-05-21 22:46:18 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿class Spiral
+{
+    static void DisplayArray(int[,] a)
+    {
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++) Console.Write("{0,3} ", a[i, j]);
+            Console.WriteLine();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            
+
+            int n = 4;
+            
+
+            Console.WriteLine();
+
+            int[,] a = new int[n, n];
+
+            int i = 0, j = 0;
+
+            int value = 1;
+
+            while (n != 0)
+            {
+                int k = 0;
+                do { a[i, j++] = value++; } while (++k < n - 1);
+                for (k = 0; k < n - 1; k++) a[i++, j] = value++;
+                for (k = 0; k < n - 1; k++) a[i, j--] = value++;
+                for (k = 0; k < n - 1; k++) a[i--, j] = value++;
+
+                ++i; ++j; n = n < 2 ? 0 : n - 2;
+            }
+
+            DisplayArray(a);
+            Console.WriteLine();
+        }
+    }
+}
